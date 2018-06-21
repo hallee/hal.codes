7c7d408bf837645c7a1bde000f223f132e528480
Hal Lee <hal@hal-lee.com>
2018-06-21 08:41:28 -0400

    Hello world Leaf template

--- Sources/App/Views/home.swift
+++ Sources/App/Views/home.swift
@@ -1,2 +1,14 @@
+import Vapor
 import Leaf
 
+struct Home: Codable {
+    var title: String
+    var message: String
+}
+
+func generateHomePage(for req: Request) throws -> Future<View> {
+    let leaf = try req.make(LeafRenderer.self)
+    let context = Home(title: "hal.codes", message: "Hello World!")
+
+    return leaf.render("home", context)
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,9 +3,9 @@ import Vapor
 public func routes(_ router: Router) throws {
     
     router.get() { req in
-        return "Hello, world!"
+        return try generateHomePage(for: req)
     }
-    
+
     /// Redirect all unknown paths to TLD
     router.get(PathComponent.anything) { req in
         return req.redirect(to: "/", type: .normal)
