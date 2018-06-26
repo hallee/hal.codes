2b7c662a9597b733a0b640a1934f3975efd118a2
Hal Lee <hal@hal-lee.com>
2018-06-26 09:40:42 -0400

    Sass compilation in Swift

--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2"),
+        .package( url: "https://github.com/robinwalterfit/Swift-Sass.git", from: "1.0.0" )
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "Sass"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
