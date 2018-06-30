b8280acfd2a6e722e446feefb47be829e9292dd6
Hal Lee <hal@hal-lee.com>
2018-06-30 17:21:05 -0400

    Playground initial support

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -2,5 +2,11 @@ import Vapor
 
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
-    // your code here
+    let playground = MicroPlayground(DirectoryConfig.detect().workDir)
+    let codeText = """
+    // testing errors:
+    print("Hello World!" * 500.0
+    """
+    let result = playground.run(code: codeText)
+    print(result)
 }
