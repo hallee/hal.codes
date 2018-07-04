7ef9fa6b5f56c1541cb89845478d5cc539084428
Hal Lee <hal@hal-lee.com>
2018-07-04 09:28:24 -0400

    Rendering 'root', will replace home

---
+++ Sources/App/Views/root.swift
@@ -0,0 +1,8 @@
+import Vapor
+import Leaf
+
+func generateVueRoot(for req: Request) throws -> Future<View> {
+    let leaf = try req.make(LeafRenderer.self)
+
+    return leaf.render("root", TemplateData.null)
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,40 +3,13 @@ import Vapor
 public func routes(_ router: Router) throws {
     
     router.get() { req -> EventLoopFuture<View> in
-        try testPlayground(req)
-        return try generateHomePage(for: req)
+        return try generateVueRoot(for: req)
     }
 
-    /// Redirect all unknown paths to TLD
+    /// Create Vue root page for all paths
+    /// Vue will handle routing client-side
     router.get(PathComponent.catchall) { req in
-        return req.redirect(to: "/", type: .normal)
+        return try generateVueRoot(for: req)
     }
     
-}
-
-func testPlayground(_ container: Container) throws {
-    let codeText = """
-    // testing errors:
-    print("Hello World!" * 500.0
-    """
-    // connect to echo.websocket.org
-    let done = try container.client().webSocket("ws://localhost:8080/playground").flatMap { ws -> Future<Void> in
-        // setup an on text callback that will print the echo
-        ws.onText { ws, text in
-            print("rec: \(text)")
-            // close the websocket connection after we recv the echo
-            ws.close()
-        }
-        
-        // when the websocket first connects, send message
-        ws.send(codeText)
-        
-        // return a future that will complete when the websocket closes
-        return ws.onClose
-    }
-    
-    print(done) // Future<Void>
-    
-    // wait for the websocket to close
-//    try done.wait()
 }
