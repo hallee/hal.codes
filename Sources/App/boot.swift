9a518e64780a7fe488cd82ad9cf2f23f0dc02e76
Hal Lee <hal@hal-lee.com>
2018-07-03 21:41:40 -0400

    Split up scripts, updated fonts

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -2,11 +2,5 @@ import Vapor
 
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
-    let playground = MicroPlayground(DirectoryConfig.detect().workDir)
-    let codeText = """
-    // testing errors:
-    print("Hello World!" * 500.0
-    """
-    let result = playground.run(code: codeText)
-    print(result)
+
 }
