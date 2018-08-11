7a383e5fd4beac652d0e7f9086d9bf10580d3d4e
Hal Lee <hal@hal-lee.com>
2018-08-11 07:43:00 -0400

    Added router!

---
+++ Frontend/Views/Project.vue
@@ -0,0 +1,12 @@
+<template>
+	<h2>Project '{{ $route.params.project }}'</h2>
+</template>
+
+<script>
+export default {
+	beforeRouteEnter (to, from, next) {
+		// Check that Markdown file exists for project, otherwise:
+		next('/')
+	}
+}
+</script>
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import Container from './Views/Container.vue'
+import Project from './Views/Project.vue'
 
+Vue.use(VueRouter)
 Vue.use(VueResource)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Container
+        },
+        {
+            path: '/:project',
+            component: Project
+        }
+    ]
+})
 
 var vm = new Vue({
     el: '#app',
-    template: '<Container/>',
-	components: { Container }
+    template: '<router-view></router-view>',
+    router
 })
