4820fa67115e842848ad6c17b9c0b9e793321088
hallee <hal@hal-lee.com>
2018-11-26 17:22:39 -0500

    Back on babel, using more modern config.js

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,13 @@ module.exports = {
         loader: 'vue-loader'
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
         test: /\.md$/,
         use: [
           {
