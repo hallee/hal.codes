7f4e3027bca1e658378c59f4aaf48b296dbc64bc
Hal Lee <hal@lee.me>
2018-11-30 07:42:54 -0500

    locally grown fonts

--- Frontend/Views/Home.vue
+++ Frontend/Views/Home.vue
@@ -27,15 +27,15 @@ export default {
   font-family: 'PlexSans';
   font-style: italic;
   font-weight: 400;
-  src: url(https://fonts.gstatic.com/s/ibmplexsans/v2/zYX-KVElMYYaJe8bpLHnCwDKhdTuF6ZJW9XjDg.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  src: url('/fonts/ibmplexsans-italic.woff2') format('woff2'),
+       url('/fonts/ibmplexsans-italic.ttf') format('truetype');
 }
 @font-face {
   font-family: 'PlexSans';
   font-style: normal;
   font-weight: 400;
-  src: url(https://fonts.gstatic.com/s/ibmplexsans/v2/zYXgKVElMYYaJe8bpLHnCwDKhdHeFaxOedc.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  src: url('/fonts/ibmplexsans-regular.woff2') format('woff2'),
+       url('/fonts/ibmplexsans-regular.ttf') format('truetype');
 }
 @font-face {
   font-family: 'Iosevka';
