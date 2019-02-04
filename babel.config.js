c386569c479bed054c673ac1fe910173d62ac722
Hal Lee <hal@lee.me>
2019-02-04 08:32:46 -0500

    An attempt, but no polyfills are being applied

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,32 @@
 module.exports = {
-  presets: [[
-    '@babel/preset-env',
-    {
-      useBuiltIns: 'usage'
-    }
-  ]],
-  plugins: ['@babel/plugin-syntax-dynamic-import']
+	env: {
+		legacy: {
+			presets: [[
+				'@babel/preset-env', {
+					modules: false,
+					useBuiltIns: 'usage',
+					targets: [
+						'>0.2%',
+						'safari >= 5',
+						'ios >= 6'
+					],
+					debug: true
+				}
+			]],
+			plugins: ['@babel/plugin-syntax-dynamic-import']
+		},
+		modern: {
+			presets: [[
+				'@babel/preset-env', {
+					modules: false,
+					useBuiltIns: 'usage',
+					targets: {
+						esmodules: true
+					},
+					debug: true
+				}
+			]],
+			plugins: ['@babel/plugin-syntax-dynamic-import']
+		}
+	}
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,76 +2,118 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 
-module.exports = {
-  entry: ['./Frontend/Sass/normalize.scss', './Frontend/index.js'],
-  output: {
+const mode = process.env.NODE_ENV !== 'development' ? 'production' : 'development'
+
+function commonConfig(babelLoader, output) {
+  return {
+    mode: mode,
+    entry: ['./Frontend/Sass/normalize.scss', './Frontend/index.js'],
+    output,
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            'vue-style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: { safe: true }
+              }
+            },
+            { 
+              loader: 'sass-loader',
+              options: {
+                includePaths: [path.resolve(__dirname, 'Frontend/Sass'),],
+                data: '@import "../Sass/mixins.scss";'
+              } 
+            }
+          ]
+        },
+        babelLoader,
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            productionMode: mode === 'production'
+          }
+        },
+        {
+          test: /\.md$/,
+          use: [
+            {
+              loader: 'vue-loader',          
+              options: {
+                productionMode: mode === 'production'
+              }
+            },
+            {
+              loader: 'vue-markdown-loader/lib/markdown-compiler',
+              options: {
+                raw: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    optimization: {
+      minimize: true
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new CompressionPlugin({
+        algorithm: 'gzip'
+      })
+    ]
+  }
+}
+
+const modernConfig = commonConfig(
+  {
+    test: /\.m?js$/i,
+    exclude: file => (
+      /node_modules/.test(file) &&
+      !/\.vue\.js/.test(file)
+    ),
+    use: {
+      loader: 'babel-loader',
+      options: {
+        envName: 'modern'
+      }
+    }
+  },
+  {
+    path: path.resolve(__dirname, 'Public/scripts/'),
+    publicPath: '/scripts/',
+    filename: 'bundle.mjs'
+  }
+)
+
+const legacyConfig = commonConfig(
+  {
+    test: /\.js$/i,
+    exclude: file => (
+      /node_modules/.test(file) &&
+      !/\.vue\.js/.test(file)
+    ),
+    use: {
+      loader: 'babel-loader',
+      options: {
+        envName: 'legacy'
+      }
+    }
+  },
+  {
     path: path.resolve(__dirname, 'Public/scripts/'),
     publicPath: '/scripts/',
     filename: 'bundle.js'
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          'vue-style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              minimize: { safe: true }
-            }
-          },
-          { 
-            loader: 'sass-loader',
-            options: {
-              includePaths: [path.resolve(__dirname, 'Frontend/Sass'),],
-              data: '@import "../Sass/mixins.scss";'
-            } 
-          }
-        ]
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: file => (
-          /node_modules/.test(file) &&
-          !/\.vue\.js/.test(file)
-        ),
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.md$/,
-        use: [
-          {
-            loader: 'vue-loader'
-          },
-          {
-            loader: 'vue-markdown-loader/lib/markdown-compiler',
-            options: {
-              raw: true
-            }
-          }
-        ]
-      }
-    ]
-  },
-  optimization: {
-    minimize: true
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new CompressionPlugin({
-      algorithm: 'gzip'
-    })
-  ]
-}
+  }
+)
+
+module.exports = [legacyConfig, modernConfig]
