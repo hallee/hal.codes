377d19b1c7d44e30869f57e1622dca07ebd4dd4b
Hal Lee <hal@lee.me>
2019-02-06 07:46:51 -0500

    giving up on differential serving

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,15 @@
 module.exports = {
-	env: {
-		legacy: {
-			presets: [[
-				'@babel/preset-env', {
-					modules: false,
-					useBuiltIns: 'usage',
-					targets: [
-						'>0.2%',
-						'safari >= 5',
-						'ios >= 6'
-					],
-					debug: true
-				}
-			]],
-			plugins: ['@babel/plugin-syntax-dynamic-import']
-		},
-		modern: {
-			presets: [[
-				'@babel/preset-env', {
-					modules: false,
-					useBuiltIns: 'usage',
-					targets: {
-						esmodules: true
-					},
-					debug: true
-				}
-			]],
-			plugins: ['@babel/plugin-syntax-dynamic-import']
+	presets: [[
+		'@babel/preset-env', {
+			modules: false,
+			useBuiltIns: 'usage',
+			targets: [
+				'>0.2%',
+				'safari >= 5',
+				'ios >= 6'
+			],
+			debug: true
 		}
-	}
+	]],
+	plugins: ['@babel/plugin-syntax-dynamic-import']
 };
